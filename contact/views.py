ba62f744330095d000fdf53b9a97bbd8c9d38d05
beccauwu <rebecca@perttula.co>
2022-07-25 21:15:09 +0200

    🏗️ work on email feature

--- contact/views.py
+++ contact/views.py
@@ -8,19 +8,26 @@ def contact(request):
 	if request.method == 'POST':
 		form = ContactForm(request.POST)
 		if form.is_valid():
-			subject = form.cleaned_data['subject']
+			name = form.cleaned_data['name']
+			subject = f"{name.upper()} | {form.cleaned_data['subject']}"
+			email = form.cleaned_data['email_address']
+			message = form.cleaned_data['message']
+			ebreak = '--------------------------------'
+			greeting = f"Hello {name}!\n"
+			content = "Your inquiry has been received and I'll get back to you as soon as possible!"
+			signature = 'Best,\nRebecca Perttula'
 			body = {
-			'first_name': form.cleaned_data['name'], 
-			'email': form.cleaned_data['email_address'], 
-			'message':form.cleaned_data['message'], 
+			'greeting': greeting,
+			'content': content,
+			'br1': ebreak,
+			'message': f"Your inquiry:\n{message}",
+			'signature': signature,
 			}
 			message = "\n".join(body.values())
-
 			try:
 				send_mail(subject, message, 'rebecca@perttula.co', ['rebecca@perttula.co'])
 			except BadHeaderError:
 				return HttpResponse('Invalid header found.')
 			return redirect ("home")
-      
 	form = ContactForm()
 	return render(request, "contact.html", {'form':form})
