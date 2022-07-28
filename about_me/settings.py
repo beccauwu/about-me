03815c29d929a068f7e8ee6b30eb67ae7e94c8c5
beccauwu <rebecca@perttula.co>
2022-07-28 14:23:42 +0200

    🔒️ use tls for email

--- about_me/settings.py
+++ about_me/settings.py
@@ -141,7 +141,7 @@ EMAIL_HOST = 'mail.smtp2go.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('PORT')
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
--- contact/views.py
+++ contact/views.py
@@ -3,7 +3,51 @@ from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from about_me import views
 # Create your views here.
+
+def html_message(name, message):
+    body = f"""
+		<div style='text-align: center; border: 2px solid black; border-radius: 15px; width: 250px; margin: 0 auto; padding: 40px;'>
+		<h3>Hi {name}!</h3>
+		<p>I've received your inquiry and will get back to you as soon as possible.</p>
+		<div style='padding-top:10px;border-top:2px solid black;width:200px;margin:40px auto 15px auto'></div>
+		<div style='border: 2px solid black; border-radius: 15px;'>
+		<div style='border-bottom: 2px solid black; width: 120px; margin: 40px auto;'>
+		<p>Your message:</p>
+		</div>
+		<div style='padding: 5px 10px; border-left: 4px solid #d0d0d0; width:180px; margin: 0 auto;'>
+		<p><em>{message}</em></p>
+		</div>
+		<div style='border-top: 2px solid black; width: 120px; margin: 40px auto;'></div>
+		</div>
+		<div style='margin: 5px auto;'></div>
+		<p>In the meantime if you have anything else you would like to add, feel free to reply to this message instead of submitting a new inquiry.</p>
+		<div style='padding-top:10px;border-top:2px solid black;width:200px;margin:40px auto 0 auto'>
+		<h4>Best,</h4>
+		<p>Rebecca Perttula</p>
+		<p><a href='mailto:rebecca@perttula.co'>rebecca@perttula.co</a></p>
+		<p><a href='tel:+46724416869'>+46724416869</a></p>
+		</div>
+		</div>
+    """
+    return body
+
+def text_message(name, message):
+    body = f"""
+		Hi {name}!\n
+  		I've received your inquiry and will get back to you as soon as possible.\n
+    	Your message:\n
+     	{message}\n
+		In the meantime if you have anything else you would like to add, 
+  		feel free to reply to this message instead of submitting a new inquiry.\n
+     	Best,\n
+      	Rebecca Perttula\n
+		rebecca@perttula.co\n
+		+46724416869
+    """
+    return body
+
 @csrf_exempt
 def contact(request):
 	if request.method == 'POST':
@@ -13,20 +57,8 @@ def contact(request):
 			subject = f"{name.upper()} | {form.cleaned_data['subject']}"
 			to_email = form.cleaned_data['email_address']
 			message = form.cleaned_data['message']
-			pstart = "<p style='text-align: center;'>"
-			pend = "</p>"
-			body = {
-			'greeting': f"<h3 style='text-align: center;'>Hello {name},</h3>",
-			'content': f'{pstart}I have received your inquiry and will respond as soon as possible.{pend}',
-			'br1': '--------------------------------',
-			'strong': f'{pstart}<strong>Your inquiry:</strong></p>{pend}',
-			'divstart': "<div style='padding: 12px; border-left: 4px solid #d0d0d0; width: fit-content; margin: 0 auto;'>",
-			'message': f"<p style='font-style: italic; text-align: center;'>{message}</p>",
-			'divend': '</div>',
-			'signature': 'Best,<br>Rebecca Perttula',
-			}
-			html_content = "<br>".join(body.values())
-			text_content = f"Hi {name}!\nI've received your message and will get back to you asap:\nYour message:\n{message}\nBest,\nRebecca Perttula"
+			html_content = html_message(name, message)
+			text_content = text_message(name, message)
 			try:
 				send_mail(
         			subject=subject,
@@ -40,3 +72,5 @@ def contact(request):
 			return redirect ("home")
 	form = ContactForm()
 	return render(request, "contact.html", {'form':form})
+
+
