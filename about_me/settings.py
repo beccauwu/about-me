4c0753e184ca203ba98515afe9275206e56af819
beccauwu <rebecca@perttula.co>
2022-08-04 22:11:49 +0200

    🚀 move to postgresql

--- about_me/settings.py
+++ about_me/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nsza&i6_txz$b-(96)#50y8ms6d8=&f6d3(!dma#@!q#pfcr*9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'apps.home'
 LOGOUT_REDIRECT_URL = 'apps.home'
 
-EMAIL_HOST = 'mail.smtp2go.com'
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('PORT')
