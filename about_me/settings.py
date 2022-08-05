36bb2bf6e4e7e9351f41627cd2e0b8d6723253cf
beccauwu <rebecca@perttula.co>
2022-08-05 13:12:56 +0200

    🎨  move email tls config to env

--- about_me/settings.py
+++ about_me/settings.py
@@ -154,7 +154,7 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('PORT')
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
