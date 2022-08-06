769bba4ff4c84790be66db804ad40b2e929fceca
beccauwu <rebecca@perttula.co>
2022-08-06 14:48:15 +0200

    🚑 change staticroot

--- about_me/settings.py
+++ about_me/settings.py
@@ -141,7 +141,7 @@ AWS_LOCATION = 'static'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(STATIC_URL, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
