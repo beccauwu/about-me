2b268431c8f4e02621e67d676166629deef06d81
beccauwu <rebecca@perttula.co>
2022-08-12 16:17:18 +0200

    🐛 temporary fix

--- accounts/models.py
+++ accounts/models.py
@@ -13,8 +13,7 @@ class Profile(models.Model):
         return self.user.username
 
 @receiver(post_save, sender=User)
-def update_profile_signal(sender, instance, created, login, **kwargs):
-    if not login:
-        if created:
-            Profile.objects.create(user=instance)
-        instance.profile.save()
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
