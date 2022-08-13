d932f44b3bf29f243934d8239321411ee4986808
beccauwu <rebecca@perttula.co>
2022-08-13 16:51:55 +0200

    🐛 wrong filename in views

--- accounts/views.py
+++ accounts/views.py
@@ -46,7 +46,7 @@ def profile(request):
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileForm(instance=request.user.profile)
-    return render(request, 'profile_update.html', {'user_form': user_form, 'profile_form': profile_form})
+    return render(request, 'profile.html', {'user_form': user_form, 'profile_form': profile_form})
 
 def logout_request(request):
     logout(request)
