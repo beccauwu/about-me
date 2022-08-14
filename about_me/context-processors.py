a55ceaf23d40e0613d402c63245a0924079c85bb
beccauwu <rebecca@perttula.co>
2022-08-14 16:39:52 +0200

    🎨 login form as middleware

--- about_me/context-processors.py
+++ about_me/context-processors.py
@@ -1,7 +1,7 @@
 from accounts.forms import LoginForm, NewUserForm
+        
 
 def add_account_forms(request):
     return {
-        'login_form': LoginForm(),
         'signup_form': NewUserForm()
         }
---
+++ about_me/middlewares.py
@@ -0,0 +1,14 @@
+from accounts.forms import LoginForm
+
+class LoginFormMiddleware(object):
+    def process_request(self, request):
+        if request.method == 'POST':
+            form = LoginForm(request=request, data=request.POST)
+            if form.is_valid():
+                form.user_login()
+                if '/user/logout/' in request.get_full_path():
+                    from django.shortcuts import HttpResponseRedirect
+                    return HttpResponseRedirect('/')
+        else:
+            form = LoginForm(request)
+        request.login_form = form
