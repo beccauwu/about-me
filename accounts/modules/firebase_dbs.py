0fd5294a892ba8942e8af5d1bf3d76e49cfd11e5
beccauwu <rebecca@perttula.co>
2022-08-15 15:56:59 +0200

    🐛 csrf token to profile edit form

--- accounts/modules/firebase_dbs.py
+++ accounts/modules/firebase_dbs.py
@@ -5,31 +5,33 @@ db_app = initialize_app(CREDENTIALS, {'projectId': config('FB_PROJECT_ID')})
 
 db = firestore.client(db_app)
 
-class CustomUser:
-    def __init__(self, email, username, uid, bio=None, pfp=None):
-        self.email = email
+class Profile:
+    def __init__(self, username, bio=None, pfp=None):
         self.username = username
-        self.uid = uid
         self.bio = bio
         self.pfp = pfp
     def create(self):
-        doc_ref = db.collection(u'users').document(self.uid)
+        doc_ref = db.collection(u'users').document(self.username)
         doc_ref.set({
-            u'email': self.email,
-            u'username': self.username,
-            u'uid': self.uid,
             u'bio': self.bio,
             u'pfp': self.pfp
         })
     def update(self):
-        doc_ref = db.collection(u'users').document(self.uid)
+        doc_ref = db.collection(u'users').document(self.username)
         doc_ref.update({
-            u'email': self.email,
-            u'username': self.username,
-            u'uid': self.uid,
             u'bio': self.bio,
             u'pfp': self.pfp
         })
     def delete(self):
-        doc_ref = db.collection(u'users').document(self.uid)
+        doc_ref = db.collection(u'users').document(self.username)
         doc_ref.delete()
+    def add_image(self, image_path, image_name, text='empty'):
+        doc_ref = db.collection(u'users').document(self.username)
+        doc_ref.update({
+            u'images': {
+                image_name : {
+                    u'path': image_path,
+                    u'text': text
+                },
+            },
+        })
