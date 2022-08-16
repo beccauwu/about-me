34f1023e8830b9d4f501f1d4f7190c03d883e676
beccauwu <rebecca@perttula.co>
2022-08-16 15:46:19 +0200

    🐛 try to fix styles not importing

--- photography/models.py
+++ photography/models.py
@@ -11,18 +11,6 @@ def upload_location(instance, filename):
 
 # Create your models here.
 
-class PhotoGallery(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    collection = models.CharField(max_length=200)
-    img = models.FileField(upload_to =f'pics/gallery/{collection}')
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural = "Photo Galleries"
-
-    def __str__(self):
-        return self.title
-
 class Collection(models.Model):
     collection = models.CharField(max_length=200, unique=True)
     collection_summary = models.CharField(max_length=200)
@@ -35,11 +23,11 @@ class Collection(models.Model):
 
 class Image(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    collection = models.ForeignKey(Collection, default=1, verbose_name='Collections', on_delete=models.SET_DEFAULT)
-    likes = models.ManyToManyField(User, related_name='blogpost_like')
+    likes = models.ManyToManyField(User)
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
     img = models.ImageField(upload_to=upload_location)
     upload_date = models.DateTimeField(auto_now_add=True)
+    collection = models.ForeignKey(Collection, default=1, verbose_name='Collections', on_delete=models.SET_DEFAULT)
 
     def number_of_likes(self):
         return self.likes.count()
@@ -49,9 +37,20 @@ class Image(models.Model):
 
 class Comment(models.Model):
     comment = models.CharField(max_length=200)
-    image = models.ForeignKey(Image, default=1, verbose_name='Images', on_delete=models.SET_DEFAULT)
-    author = models.ForeignKey(User, default=1, verbose_name='Users', on_delete=models.SET_DEFAULT)
+    author = models.OneToOneField(User, default=1, on_delete=models.SET_DEFAULT)
     created_on = models.DateTimeField(auto_now_add=True)
+    image = models.ForeignKey(Image, default=1, verbose_name='Images', on_delete=models.SET_DEFAULT)
 
     def __str__(self):
         return self.comment
+
+class PhotoGallery(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    collection = models.ForeignKey(Collection, default=1, on_delete=models.SET_DEFAULT)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Photo Galleries"
+
+    def __str__(self):
+        return self.title
