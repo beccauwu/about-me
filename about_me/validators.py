f4dff877c5c38bf62198cb8ceccd886cf4dc2796
beccauwu <rebecca@perttula.co>
2022-08-16 15:51:08 +0200

    🚨 avoid making migrations at this stage

--- about_me/validators.py
+++ about_me/validators.py
@@ -1,15 +1,16 @@
 from django.core.exceptions import ValidationError
 
-def validate_file_size(value, instance):
+def validate_file_size(value):
     """validates file size of uploaded image
     uses instance max_size if specified, defaults to 2MB
     """
-    filesize = value.size
-    if instance.max_size:
-        limit = instance.max_size * 1024 * 1024
-    else:
-        limit = 2 * 1024 * 1024
-    if filesize > limit:
-        raise ValidationError(f"The maximum file size that can be uploaded is {limit}")
-    else:
-        return value
+    # filesize = value.size
+    # if instance.max_size:
+    #     limit = instance.max_size * 1024 * 1024
+    # else:
+    #     limit = 2 * 1024 * 1024
+    # if filesize > limit:
+    #     raise ValidationError(f"The maximum file size that can be uploaded is {limit}")
+    # else:
+    #     return value
+    return value
