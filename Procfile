b27518bc18c43000b30a4168a1e62ab29547a342
beccauwu <rebecca@perttula.co>
2022-08-21 16:07:05 +0200

    rm cached files

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: gunicorn about_me.wsgi:application --log-file - --log-level debug
-heroku ps:scale web=1
-python manage.py migrate
--- about_me/context-processors.py
+++
@@ -1,9 +0,0 @@
-from accounts.forms import LoginForm, NewUserForm
-from django.contrib.auth.models import User
-        
-
-def add_account_forms(request):
-    return {
-        'signup_form': NewUserForm(),
-        'users': User.objects.all()
-        }
