1519cb67ae18405742c7c408170cddd6895fc424
beccauwu <rebecca@perttula.co>
2022-08-22 01:12:58 +0200

    ✨ create like model

--- photography/models.py
+++ photography/models.py
@@ -25,10 +25,20 @@ class Image(models.Model):
     title = models.CharField(max_length=200, unique=True)
     img = models.ImageField(upload_to=upload_location)
     upload_date = models.DateTimeField(auto_now_add=True)
+    text_content = models.TextField(blank=True)
     collection = models.ForeignKey(Collection, default=1, verbose_name='Collections', on_delete=models.SET_DEFAULT)
 
     def __str__(self):
         return self.title
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'image'], name='unique_like')
+        ]
 
 class Comment(models.Model):
     comment = models.TextField()
