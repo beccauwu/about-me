6b5dee7833ac2898266ba62a8ea02e1dd4c60e51
beccauwu <rebecca@perttula.co>
2022-08-22 22:06:23 +0200

    🚀 add pwa to urls

--- about_me/urls.py
+++ about_me/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic.base import TemplateView
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pwa.urls')),
     path('', include('photography.urls')),
     path('contact/', include('contact.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
