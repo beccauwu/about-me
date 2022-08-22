55de21886083f2fa5a6b59c1e56e61f47f9a6a95
beccauwu <rebecca@perttula.co>
2022-08-22 22:04:17 +0200

    🐛 add loginform into context

--- about_me/context-processors.py
+++ about_me/context-processors.py
@@ -5,5 +5,6 @@ from django.contrib.auth.models import User
 def add_account_forms(request):
     return {
         'signup_form': NewUserForm(),
+        'login_form': LoginForm(),
         'users': User.objects.all()
         }
